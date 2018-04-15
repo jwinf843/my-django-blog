89fc3f4087951e55cfdcdf4113f6b1c2b09e7922
jwinf843 <jwinf843@gmail.com>
2018-04-15 12:58:37 +0000

    QOL Updates mades

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,15 @@
 from django.conf.urls import url 
 from . import views
 
+#    The Preview Commands: 
+#        python manage.py runserver 0.0.0.0:8080
+
 # URL Patterns
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     ]
     
